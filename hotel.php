6fd0e79f115ef957b409a89cf76eac2acf738457
Femilya Juniar <femilyaniar222@gmail.com>
2020-11-09 17:59:29 +0700

    folder hotel sudah siap

--- hotel.php
+++ hotel.php
@@ -50,6 +50,37 @@ $datasingle6["nomor_telpon"] = "123456789012";
 $datasingle6["kordinat"] = "3.597620, 98.831597";
 $datasingle6["gambar_url"] = "http://localhost/apifemi/foto/6hotel_travel.jpeg";
 
+$datasingle7 = array();
+$datasingle7["id"] = "7";
+$datasingle7["nama"] =  "Thong's Inn Hotel Kualanamu";
+$datasingle7["alamat"] = "Jalan Pasar V Kebun Kelapa, Medan, Deli Serdang, Sumatera Utara, Indonesia, 20552";
+$datasingle7["nomor_telpon"] = "123456789012";
+$datasingle7["kordinat"] = "3.593010, 98.861152";
+$datasingle7["gambar_url"] = "http://localhost/apifemi/foto/7hotel_inn.jpeg";
+
+$datasingle8 = array();
+$datasingle8["id"] = "8";
+$datasingle8["nama"] =  "Hotel Aero";
+$datasingle8["alamat"] = "Jalan. Paya Pasir No. 8 B-N, Tanjung Morawa Deli Serdang. Sumatra Utara, Medan, Deli Serdang, Sumatera Utara, Indonesia, 20362";
+$datasingle8["nomor_telpon"] = "123456789012";
+$datasingle8["kordinat"] = "3.521169, 98.784966";
+$datasingle8["gambar_url"] = "http://localhost/apifemi/foto/8hotel_aero.jpeg";
+
+$datasingle9 = array();
+$datasingle9["id"] = "9";
+$datasingle9["nama"] =  "My Studio Hotel Kualanamu Airport Medan";
+$datasingle9["alamat"] = "Jl. Sultan Serdang, Tumpatan Nibung, Kec. Batang Kuis, Kabupaten Deli Serdang, Sumatera Utara 20372";
+$datasingle9["nomor_telpon"] = "123456789012";
+$datasingle9["kordinat"] = "3.598012, 98.831834";
+$datasingle9["gambar_url"] = "http://localhost/apifemi/foto/9hotel_studio.jpeg";
+
+$datasingle10 = array();
+$datasingle10["id"] = "10";
+$datasingle10["nama"] =  "Pancur Gading Hotel & Resort";
+$datasingle10["alamat"] = "Jalan Kuala Simeme No. 1 Pamah, Delitua, Medan, Deli Serdang, Sumatera Utara, Indonesia, 20356";
+$datasingle10["nomor_telpon"] = "123456789012";
+$datasingle10["kordinat"] = "3.473196, 98.680152";
+$datasingle10["gambar_url"] = "http://localhost/apifemi/foto/10hotel_gading.jpeg";
 
 array_push($response["hotel"], $datasingle1);
 array_push($response["hotel"], $datasingle2);
