7db3324f50840368b2bc252bcd494545e9a9a62c
Femilya Juniar <femilyaniar222@gmail.com>
2020-11-10 21:23:45 +0700

    tambah file wisata

---
+++ wisata.php
@@ -0,0 +1,47 @@
+{
+	"wisata": [
+	{
+		"id": "1";
+		"nama": "Danau Linting";
+		"gambar_url": "http://localhost/apifemi/foto/1wisata_linting.jpg";
+		"kategori": "Alam"
+	};
+	{
+		"id": "2";
+		"nama": "The Le Hu Garden";
+		"gambar_url": "http://localhost/apifemi/foto/2wisata_th.jpg";
+		"kategori": "Taman"
+	};
+	{
+		"id": "3";
+		"nama": "Air Terjun Sampuren Putih";
+		"gambar_url": "http://localhost/apifemi/foto/3wisata_air.jpg";
+		"kategori": "Alam"
+	};
+	{
+		"id": "4";
+		"nama": "Pantai Salju";
+		"gambar_url": "http://localhost/apifemi/foto/4wisata_salju.jpg";
+		"kategori": "Alam"
+	};
+	{
+		"id": "5";
+		"nama": "Air Terjun Pelangi";
+		"gambar_url": "http://localhost/apifemi/foto/5wisata_pelangi.jpg";
+		"kategori": "Alam"
+	};
+	{
+		"id": "6";
+		"nama": "Taman Pramuka Sibolangit";
+		"gambar_url": "http://localhost/apifemi/foto/6wisata_pramuka.jpg";
+		"kategori": "Taman"
+	};
+	{
+		"id": "7";
+		"nama": "Kampung Ladang Outbond";
+		"gambar_url": "http://localhost/apifemi/foto/6wisata_ladang.jpg";
+		"kategori": "Taman"
+	};
+
+	]
+}
