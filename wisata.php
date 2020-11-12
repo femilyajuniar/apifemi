b6d913bf9addff2dff7fcd019af7356566ec89a5
Femilya Juniar <femilyaniar222@gmail.com>
2020-11-12 10:08:59 +0700

    pembaharuan file wisata

--- wisata.php
+++ wisata.php
@@ -1,47 +1,58 @@
-{
-	"wisata": [
-	{
-		"id": "1";
-		"nama": "Danau Linting";
-		"gambar_url": "http://localhost/apifemi/foto/1wisata_linting.jpg";
-		"kategori": "Alam"
-	};
-	{
-		"id": "2";
-		"nama": "The Le Hu Garden";
-		"gambar_url": "http://localhost/apifemi/foto/2wisata_th.jpg";
-		"kategori": "Taman"
-	};
-	{
-		"id": "3";
-		"nama": "Air Terjun Sampuren Putih";
-		"gambar_url": "http://localhost/apifemi/foto/3wisata_air.jpg";
-		"kategori": "Alam"
-	};
-	{
-		"id": "4";
-		"nama": "Pantai Salju";
-		"gambar_url": "http://localhost/apifemi/foto/4wisata_salju.jpg";
-		"kategori": "Alam"
-	};
-	{
-		"id": "5";
-		"nama": "Air Terjun Pelangi";
-		"gambar_url": "http://localhost/apifemi/foto/5wisata_pelangi.jpg";
-		"kategori": "Alam"
-	};
-	{
-		"id": "6";
-		"nama": "Taman Pramuka Sibolangit";
-		"gambar_url": "http://localhost/apifemi/foto/6wisata_pramuka.jpg";
-		"kategori": "Taman"
-	};
-	{
-		"id": "7";
-		"nama": "Kampung Ladang Outbond";
-		"gambar_url": "http://localhost/apifemi/foto/6wisata_ladang.jpg";
-		"kategori": "Taman"
-	};
+<?php
 
-	]
-}
+$response = array();
+$response["wisata"] = array();
+$datasingle1 = array();
+$datasingle1["id"] = "1";
+$datasingle1["nama"] = "Danau Linting";
+$datasingle1["gambar_url"] = "http://localhost/apifemi/foto/1wisata_linting.jpg";
+$datasingle1["kategori"] = "Alam";
+
+$datasingle2 = array();
+$datasingle2["id"] = "2";
+$datasingle2["nama"] = "The Le Hu Garden";
+$datasingle2["gambar_url"] = "http://localhost/apifemi/foto/2wisata_th.jpg";
+$datasingle2["kategori"] = "Taman";
+
+$datasingle3 = array();
+$datasingle3["id"] = "3";
+$datasingle3["nama"] = "Air Terjun Sampuren Putih";
+$datasingle3["gambar_url"] = "http://localhost/apifemi/foto/3wisata_air.jpg";
+$datasingle3["kategori"] = "Alam";
+
+$datasingle4 = array();
+$datasingle4["id"] = "4";
+$datasingle4["nama"] = "Pantai Salju";
+$datasingle4["gambar_url"] = "http://localhost/apifemi/foto/4wisata_salju.jpg";
+$datasingle4["kategori"] = "Alam";
+	
+$datasingle5 = array();
+$datasingle5["id"] = "5";
+$datasingle5["nama"] = "Air Terjun Pelangi";
+$datasingle5["gambar_url"] = "http://localhost/apifemi/foto/5wisata_pelangi.jpg";
+$datasingle5["kategori"] = "Alam";
+
+$datasingle6 = array();
+$datasingle6["id"] = "6";
+$datasingle6["nama"] = "Taman Pramuka Sibolangit";
+$datasingle6["gambar_url"] = "http://localhost/apifemi/foto/6wisata_pramuka.jpg";
+$datasingle6["kategori"] = "Taman";
+
+$datasingle7 = array();
+$datasingle7["id"] = "7";
+$datasingle7["nama"] = "Kampung Ladang Outbond";
+$datasingle7["gambar_url"] = "http://localhost/apifemi/foto/6wisata_ladang.jpg";
+$datasingle7["kategori"] = "Taman";
+
+array_push($response["wisata"], $datasingle1);
+array_push($response["wisata"], $datasingle2);
+array_push($response["wisata"], $datasingle3);
+array_push($response["wisata"], $datasingle4);
+array_push($response["wisata"], $datasingle5);
+array_push($response["wisata"], $datasingle6);
+array_push($response["wisata"], $datasingle7);
+
+echo json_encode($response);
+
+?>
+
---
+++ wisatadetail.php
@@ -0,0 +1,74 @@
+<?php
+if(isset($_GET['id'])){
+
+    $id = $_GET['id'];
+    $response = array();
+
+    if($id == "1"){
+
+        $response["id"] = 1;
+        $response["nama"] = "Danau Linting";
+        $response["kordinat"] = "3.229786, 98.725824";
+        $response["deskripsi"] = "Danau Linting adalah sebuah danau yang terbentuk akibat retakan vulkanik bukit yang mengeluarkan air panas. Danau yang memiliki luas 1 hektar ini terkenal akan pesona magisnya. Pemandangan hijau khas pohon beringin akan menyambut kehadiran para wisatawan yang datang. Kawasan danau yang begitu asri membuat wisatawan betah disini. Danau ini juga memiliki keunikan tersendiri. Konon, tidak ada yang tau berapa kedalaman danau ini.";
+    } else if($id == "2"){
+        $response["id"] = 2;
+        $response["nama"] = "Excelso - Bandara Internasional Kualanamu";
+        $response["kordinat"] = "3.597735, 98.750344";
+        $response["deskripsi"] = "Menjual western food, coffee. Merupakan tempat santai dengan design interior modern";
+    } else if($id == "3"){
+        $response["id"] = 3;
+        $response["nama"] = "Budaya Resto";
+        $response["alamat"] = "Jl. Raya Medan Jl. Lokasi No.KM. 12, RW.5, Ujung Serdang, Tj. Morawa, Kabupaten Deli Serdang, Sumatera Utara 20362";
+        $response["jam_buka_tutup"] = "Setiap Hari Pukul 10.00 WIB - 22.00 WIB";
+        $response["kordinat"] = "3.597735, 98.750344";
+        $response["gambar_url"] = "http://localhost/apifemi/foto/3kuliner_budaya.jpeg";
+        $response["kategori"] = "Kuliner Inovasi";
+        $response["nomor_telp"] = "0264700142";
+        $response["deskripsi"] = "Jalur lintas antara Medan - Kualanamu pun mulai diramaikan oleh beberapa resto yang cocok buat kamu singgah saat lapar di tengah jalan, salah satunya adalah resto yang kaya akan budaya yaitu Budaya Resto Tanjung Morawa, resto yang tidak terlalu jauh yang bisa jadi tempat 'kabur' kamu bersama teman ataupun keluarga.";
+    } else if($id == "3"){
+        $response["id"] = 3;
+        $response["nama"] = "Budaya Resto";
+        $response["alamat"] = "Jl. Raya Medan Jl. Lokasi No.KM. 12, RW.5, Ujung Serdang, Tj. Morawa, Kabupaten Deli Serdang, Sumatera Utara 20362";
+        $response["jam_buka_tutup"] = "Setiap Hari Pukul 10.00 WIB - 22.00 WIB";
+        $response["kordinat"] = "3.597735, 98.750344";
+        $response["gambar_url"] = "http://localhost/apifemi/foto/3kuliner_budaya.jpeg";
+        $response["kategori"] = "Kuliner Inovasi";
+        $response["nomor_telp"] = "0264700142";
+        $response["deskripsi"] = "Jalur lintas antara Medan - Kualanamu pun mulai diramaikan oleh beberapa resto yang cocok buat kamu singgah saat lapar di tengah jalan, salah satunya adalah resto yang kaya akan budaya yaitu Budaya Resto Tanjung Morawa, resto yang tidak terlalu jauh yang bisa jadi tempat 'kabur' kamu bersama teman ataupun keluarga.";
+    }
+    else if($id == "4"){
+        $response["id"] = 4;
+        $response["nama"] = "Hotplate Kita Kita";
+        $response["alamat"] = "Jl. Mh. Thamrin, Petapahan, Kec. Lubuk Pakam, Kabupaten Deli Serdang, Sumatera Utara 20517";
+        $response["jam_buka_tutup"] = "Setiap Hari pukul 10.00 - 00.00 WIB";
+        $response["kordinat"] = "3.557559, 98.868186";
+        $response["gambar_url"] = "http://localhost/apifemi/foto/4kuliner_plate.jpeg";
+        $response["kategori"] = "Kuliner Inovasi";
+        $response["nomor_telp"] = "0264700142";
+        $response["deskripsi"] = "Hotplate Kita Kita merupakan cafe yang terletak di Lubuk Pakam, menyediakan berbagai macam menu yang dapat dinikmati saat bersantai";
+    }else if($id == "5"){
+        $response["id"] = 5;
+        $response["nama"] = "Dapur Nek Aji";
+        $response["alamat"] = "Jl. Bersama, Tj. Garbus Satu, Kec. Lubuk Pakam, Kabupaten Deli Serdang, Sumatera Utara 20518";
+        $response["jam_buka_tutup"] = "Setiap Hari pukul 10.00 - 22.00 WIB";
+        $response["kordinat"] = "3.560730, 98.879815";
+        $response["gambar_url"] = "http://localhost/apifemi/foto/5kuliner_nek.jpeg";
+        $response["kategori"] = "Kuliner Inovasi";
+        $response["nomor_telp"] = "0264700142";
+        $response["deskripsi"] = "Menyajikan masakan rumahan yang berkualitas, rasakan rumah yang selalu membuat kita rindu";
+    }else if($id == "6"){
+        $response["id"] = 6;
+        $response["nama"] = "Kawan Burger";
+        $response["alamat"] = "Jalan Raya Lintas Tengah Galang Tanjung Garbus, Tj. Garbus II, Kec. Pagar Merbau, Kabupaten Deli Serdang, Sumatera Utara 20551";
+        $response["jam_buka_tutup"] = "3.512627, 98.902140";
+        $response["kordinat"] = "3.560730, 98.879815";
+        $response["gambar_url"] = "http://localhost/apifemi/foto/6kuliner_kawan.jpeg";
+        $response["kategori"] = "Kuliner Inovasi";
+        $response["nomor_telp"] = "0264700142";
+        $response["deskripsi"] = "Kawan Burger merupakan salah satu kuliner yang terletak di desa Pagar Merbau 2, menyediakan berbagai menu burger yang membuatnya selalu nagih";
+    }
+
+   echo json_encode($response);
+
+}
+?>
